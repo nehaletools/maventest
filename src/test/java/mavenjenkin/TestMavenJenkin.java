c431c6dd6a04681f1526ac901aa83eb82891a5ad
nehaletools <nbaig@ucm.com.au>
2023-01-04 10:56:27 +0500

    created freestyle maven project for test jenkins

---
+++ src/test/java/mavenjenkin/TestMavenJenkin.java
@@ -0,0 +1,36 @@
+package mavenjenkin;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestMavenJenkin {
+    //@Parameters("Browser")
+    @Test
+    public void startBrowser()//String browserName
+    {
+        String browserName = "Chrome";
+        System.out.println("Parameter value is " + browserName);
+        WebDriver driver = null;
+
+        if (browserName.contains("Chrome")) {
+            WebDriverManager.chromedriver().setup();
+//            ChromeOptions opt = new ChromeOptions();
+//            opt.addArguments("--headless");
+//            opt.addArguments("--no-sandbox");
+//            opt.addArguments("--disable-dev-shm-usage");
+            driver = new ChromeDriver();//opt
+        } else if (browserName.contains("Edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        }
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+        Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
+        driver.quit();
+    }
+
+}
